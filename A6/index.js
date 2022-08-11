0faf27162da3baff98c45a0c98c8fd15d23edf03
Mateus Malvezzi <mmalvezzi@limaconsulting.com>
2022-08-11 17:53:30 -0300

    class six

---
+++ A6/index.js
@@ -0,0 +1,14 @@
+// Número  INTEIRO
+//var numero1 = Number.parseInt(window.prompt('Digite um número?'))
+//var numero2 = Number.parseInt(window.prompt('Digite outro número?'))
+//var soma = numero1 + numero2;
+//var result = window.alert('A soma dos valores é, ' + soma)
+
+//Número REAL
+//var numero1 = Number.parseFloat(window.prompt('Digite um número?'))
+//var numero2 = Number.parseFloat(window.prompt('Digite outro número?'))
+//var somaS = numero1 + numero2;
+//var result = window.alert('A soma dos valores é, ' + soma)
+
+// (number + number) -> Adição
+// (number + string) -> Concatenação
