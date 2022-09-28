65ca8ef11adf1be8837abbea977bae356f7fc675
Mateus Malvezzi <mmalvezzi@limaconsulting.com>
2022-09-27 22:09:13 -0300

    DOM manipulation

---
+++ Aula10/ChangeText.js
@@ -0,0 +1,13 @@
+var area = document.querySelector("#area")
+
+function clicar() {
+    area.innerText = 'Olá!'
+}
+
+function sair(){
+    area.innerText = 'Interaja'
+}
+
+function entrar(){
+    area.innerText = 'Clique'
+}
---
+++ Aula10/changeColor.js
@@ -0,0 +1,17 @@
+var area = document.querySelector("#area")
+
+function clicar() {
+    area.style.background = 'yellow'
+    area.style.color = 'black'
+}
+
+function entrar(){
+    area.style.background = 'green'
+}
+
+function sair(){
+    area.style.background = 'red'
+    area.style.color = 'white'
+
+}
+
