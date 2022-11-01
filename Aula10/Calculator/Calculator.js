1a9afbb7669af3dc5060b6dc50c2d60f59706add
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2022-10-31 21:02:21 -0300

    FIX: The whay to call the input || Now, using getElementById always.

--- Aula10/Calculator/Calculator.js
+++ Aula10/Calculator/Calculator.js
@@ -1,6 +1,6 @@
 function somar() {
-    var tn1 = document.querySelector("#txtn1");
-    var tn2= document.querySelector("#txtn2");
+    var tn1 = document.getElementById("txtn1");
+    var tn2= document.getElementById("txtn2");
     var numero1 = Number(tn1.value);
     var numero2 = Number(tn2.value);
 
@@ -8,3 +8,4 @@ function somar() {
     
     resultado.innerHTML = `A soma entre ${numero1} e ${numero2} é igual a ${Soma}`;
 }
+//I made the modification to getElementById because after some research I saw that it is the best model.  Also because, using ID, it will already be indexed, so it won't be necessary to "go through" the entire DOM to find what gives "match".
