4af72a7e11c49d3dc9c48eecf32e9f372dc0ecdb
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2022-11-01 22:54:30 -0300

    Created: First exercise A11 - Conditions Part01

---
+++ Aula11ConditionsP1/GeneralInformation.js
@@ -0,0 +1,12 @@
+//condition01
+let anything =1;
+
+if{anything}{
+    exemple: true;
+}
+//condition02
+if(anything){
+    example:true;
+}else{
+    example:false;
+}
---
+++ Aula11ConditionsP1/VelocidadeConsole.js
@@ -0,0 +1,8 @@
+let vel = 59.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}else{
+    console.log(`Siga tranquilo`)
+}
+console.log(`Dirija sempre usando cinto de segurançaS`)
